c7513816c90fdaa9c8ca1b0d60ea6e886357cc04
amos-kibet <kibetamos97@gmail.com>
2024-09-26 13:09:15 +0300

    Seed the DB

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,24 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Blog.Posts.Post
+
+# TODO: Ideally, you would want to use this code block, for performance reasons,
+# but since I want the posts to be ordered on the UI, I used sequential inserts
+# with the for-loop
+#
+# Task.async_stream(1..100, fn i ->
+#   Ash.create!(Post, %{
+#     title: "Post #{i}",
+#     body: "This is the body of post #{i}."
+#   })
+# end)
+# |> Stream.run()
+
+for i <- 1..100 do
+  Ash.create!(Post, %{
+    title: "Post #{i}",
+    body: "This is the body of post #{i}."
+  })
+end
